9de543b6805add29d83999e6290fceef8f86cac4
pavinimonga <73470093+pavinimonga@users.noreply.github.com>
2021-03-27 13:10:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ var cars, car1, car2, car3, car4;
 var track,car1img,car2img,car3img,car4img,groundimg;
 function preload(){
   track=loadImage("../images/track.jpg");
-  car1img=loadImage("../images/car1.png");
+  car1img=loadImage("images/car1.png");
   car2img=loadImage("../images/car2.png");
   car3img=loadImage("../images/car3.png");
   car4img=loadImage("../images/car4.png");
